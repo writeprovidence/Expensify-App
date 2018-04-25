21b11f735e311408dd496b6441fafefdab846003
wils wilson <owills_bmer@outlook.com>
2018-04-25 22:48:42 +0100

    Add expense summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,11 @@
+//reduce- array function.
+
+export default (expenses) => {
+  if (expenses.length === 0) {
+    return 0;
+  } else {
+  return expenses
+      .map((expense) => expense.amount)
+      .reduce((sum, value) => sum + value, 0);
+  }
+};
