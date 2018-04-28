e5bda631c57f9429f0e06200bda81be33a95c110
wils wilson <owills_bmer@outlook.com>
2018-04-28 15:22:13 +0100

    Setup test database env

--- src/action/expenses.js
+++ src/action/expenses.js
@@ -1,24 +1,49 @@
 import uuid from 'uuid';
+import database from '../firebase/firebase';
 
 //amount is use in penny..
 // ADD_EXPENSE
-export const addExpense = (
-  { 
-    description = '', 
-    note = '', 
-    amount = 0,
-    createdAt = 0
-  } = {}
-) => ({
+//export const addExpense = (
+//  { 
+//    description = '', 
+//    note = '', 
+//    amount = 0,
+//    createdAt = 0
+//  } = {}
+//) => ({
+//  type: 'ADD_EXPENSE',
+//  expense: {
+//    id: uuid(),
+//    description,
+//    note,
+//    amount,
+//    createdAt
+//  }
+//});
+export const addExpense = () => ({
   type: 'ADD_EXPENSE',
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 });
+
+export const startAddExpense =(expenseData = {}) => {
+  return (dispatch) => {
+    const { 
+      description = '', 
+      note = '', 
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+};
+
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
