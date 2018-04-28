ee8237e1125ec28feb2ce9fd97f2d2dba8c191e7
wils wilson <owills_bmer@outlook.com>
2018-04-28 17:56:17 +0100

    Setup startRemoveExpense

--- src/action/expenses.js
+++ src/action/expenses.js
@@ -49,6 +49,14 @@ export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
+//return the promise-for app.js
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
